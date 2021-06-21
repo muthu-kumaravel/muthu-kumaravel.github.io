d0ea3eb3cf8babaaa7e1f0743c2e82bd45ec43b5
muthu-kumaravel <muthukumaravel.muthuraman@gmail.com>
2021-06-21 17:26:32 +0530

    ADD : Multiple sections and modified tint and gradient of the side shadows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
