1f3557c359b69f647c1c0d6ec05117ceb0c03bb0
muthu-kumaravel <muthukumaravel.muthuraman@gmail.com>
2021-12-27 14:34:11 +0530

    FIX: Alignment Issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,4 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
-  notMobile: boolean = true ;
-  ngOnInit() {
-    if (window.screen.width < 1000 ) {
-      this.notMobile = false;
-    }
-    else {
-      this.notMobile = true;
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    if (window.screen.width < 1000 ) {
-      this.notMobile = false;
-    }
-    else {
-      this.notMobile = true;
-    }
-  }
-
 }
