d35b9a98f92e062e05a103e06952cb4dcea1507f
muthu-kumaravel <muthukumaravel.muthuraman@gmail.com>
2021-12-27 13:25:19 +0530

    ADD: About Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+  notMobile: boolean = true ;
+  ngOnInit() {
+    if (window.screen.width < 1000 ) {
+      this.notMobile = false;
+    }
+    else {
+      this.notMobile = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.screen.width < 1000 ) {
+      this.notMobile = false;
+    }
+    else {
+      this.notMobile = true;
+    }
+  }
+
 }
